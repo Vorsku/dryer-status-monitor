91a0251babe33563bf728d34bee3e5893751c616
Vorsku <vorsku@gmail.com>
2020-10-09 15:45:13 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<style>
+h2 { font-family: 'Roboto', sans-serif; text-align: center; color: white; width: auto; height: auto; padding: 20px; }
+h2.on { background-color: green; }
+h2.off { background-color: red; }
+</style>
+</head>
+
 <body>
-<a href="http://www.breakoutbros.com">
-    <img src="logo.png" alt="BreakoutBros" style="width:640px;height:225px;">
-</a>
 <p>
-<h2>
 <?php
-$status_file_name = "/home/pi/dev/vibration_kookye/python/dryer_status.txt";
+$status_file_name = "/home/pi/dryer-status-monitor/python/dryer_status.txt";
 echo nl2br(file_get_contents($status_file_name));
 ?>
-</h2>
 </body>
 </html>
